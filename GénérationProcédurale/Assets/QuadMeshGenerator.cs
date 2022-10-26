48780e5bb41ea0e75b9b72d3714992fdca328e79
GONZZY <olivier.goncalves@live.ca>
2022-10-26 10:34:01 -0400

    Notes Supplémentaire Cour 26 Oct

--- GénérationProcédurale/Assets/QuadMeshGenerator.cs
+++ GénérationProcédurale/Assets/QuadMeshGenerator.cs
@@ -17,32 +17,44 @@ public class QuadMeshGenerator : MonoBehaviour
           {
                new Vector3(-halfWidth, halfHeight),
                new Vector3(halfWidth, halfHeight),
-               new Vector3(halfWidth, 0),
-               new Vector3(-halfWidth, 0),
+               // new Vector3(halfWidth, 0),
+               // new Vector3(-halfWidth, 0),
                
-               new Vector3(-halfWidth, 0),
-               new Vector3(halfWidth, 0),
                new Vector3(halfWidth, -halfHeight),
                new Vector3(-halfWidth, -halfHeight),
+               // new Vector3(halfWidth, -halfHeight),
+               // new Vector3(-halfWidth, -halfHeight),
           };
           m.triangles = new[]
           {
                0, 1, 2, 
                2, 3, 0,
-               
-               4,5,6,
-               6,7,4
+               // 3,2,5,
+               // 5,4,3,
+               //6,7,4
           };
           // uv a la position 0 est associé avec le sommet 0
           // uv a la position 1 est associé avec le sommet 1
           // ainsi de suite...
+          
+          
+          // Les UV peuvent dépassé 1 si on veut qu'ils se dupliquent
+          // Les UV peuvent être négatifs si l'on veut flipper la figure(symétrie)
+          // Les UV peuvent être des nombres a virgules par exemple 0.5f pour montrer que la moitié du matériel
+          // 
+          
           m.uv = new Vector2[]
           {
-               new Vector2(1, 1),
                new Vector2(0, 1),
-               new Vector2(0, 0),
+               new Vector2(1, 1),
                new Vector2(1, 0),
-               new Vector2()
+               new Vector2(0, 0),
+               
+               // new Vector2(0, 1),
+               // new Vector2(1, 1),
+               // new Vector2(0, 0),
+               // new Vector2(1, 0),
+               
           };
           GetComponent<MeshFilter>().mesh = m;
           m.RecalculateNormals();
